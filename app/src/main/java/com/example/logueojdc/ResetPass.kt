12e623035e6428fa2ea349d0e59a4ff024d99a63
joseb <jose.bueno1999@hotmail.com>
2023-05-13 20:26:08 -0500

    libros

---
+++ app/src/main/java/com/example/logueojdc/ResetPass.kt
@@ -0,0 +1,60 @@
+package com.example.logueojdc
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class ResetPass : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        setContentView(R.layout.activity_reset_pass)
+        val home = findViewById<Button>(R.id.returnHome)
+        home.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+        val btnreset=findViewById<Button>(R.id.Btn_reset)
+        btnreset.setOnClickListener {
+            val email2=findViewById<TextView>(R.id.emailreset)
+            val email = email2.text.toString()
+
+            if (email.isNotEmpty()) {
+                auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "Se ha enviado un correo electrónico para restablecer la contraseña",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "La recuperación de contraseña falló",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Ingrese su correo electrónico para solicitar la recuperación de contraseña",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
+
+
+    }
+}
